961dab171ea8def32a3180484a33934fbb689ed3
Counter Logic <tuandinhbi75@gmail.com>
2024-05-20 19:34:34 +0700

    arrow

--- Assets/Scripts/Weapon/Bow.cs
+++ Assets/Scripts/Weapon/Bow.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +6,27 @@ public class Bow : MonoBehaviour, IWeapon
 {
 
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpawnPoint;
+
+    readonly int FIRE_HASH = Animator.StringToHash("Fire");
+
+    private Animator bowAnimator;
+
+    private void Awake()
+    {
+        bowAnimator = GetComponent<Animator>();
+    }
 
     public void Attack()
     {
         Debug.Log("Bow attack");
+
+        bowAnimator.SetTrigger(FIRE_HASH);
+
+        //khi bắn tên phải tạo ra 1 mũi tên mới
+        GameObject newArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
+
     }
 
     public WeaponInfo GetWeaponInfo()
